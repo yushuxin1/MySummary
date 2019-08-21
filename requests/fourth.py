a4ab93c2f07c999576a21f6facab6ab665ac04a6
yushuxin1 <3327567261@qq.com>
2019-08-21 17:44:44 +0800

    .

--- requests/fourth.py
+++ requests/fourth.py
@@ -2,8 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 r = requests.get('https://python123.io/ws/demo.html')
-print(r.text)
+# print(r.text)
 demo = r.text
 # 解析器
 soup = BeautifulSoup(demo, 'html.parser')
-print(soup.prettify())
+print(soup.title)
+# print(soup.prettify())
+tag = soup.a
+print(tag)
+print(soup.a.name)
+print(soup.a.parent.name)
+print(soup.a.parent.parent.name)
+tag1 = soup.a
+print(tag1.attrs)
+print(tag1.attrs['class'])
+print(tag1.attrs['href'])
+print(type(tag1.attrs))
+print(type(tag1))
+print(soup.a.string)
+print(soup.p)
+print(soup.p.string)
+print(type(soup.p.string))
+
+print('-----------------------------------------')
+
+newsoup = BeautifulSoup('<b><!--This is a comment--></b><p>This is not a comment</p>', 'html.parser')
+print(newsoup.b.string)
+print(type(newsoup.b.string))
+print(newsoup.p.string)
+print(type(newsoup.p.string))
